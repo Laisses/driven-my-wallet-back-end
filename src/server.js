8bfffda8a6428226a266207acc2923d27660aa4a
Laisses <laisse@gmail.com>
2022-11-15 16:59:39 -0300

    server.js: server set up

---
+++ src/server.js
@@ -0,0 +1,25 @@
+import express from "express";
+import cors from "cors";
+import dotenv from "dotenv";
+import { MongoClient } from "mongodb";
+import { routes } from "./routes.js";
+dotenv.config();
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+const mongoClient = new MongoClient(process.env.DB_URI);
+
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended:true }));
+
+const main = async () => {
+    await mongoClient.connect();
+    const db = mongoClient.db("myWallet");
+    routes(app, db);
+    app.listen(PORT, () => {
+        console.log(`server running on port ${PORT}`);
+    });
+};
+
+main();
