bab56d586d00b7375713947fd432c3145356a9dc
Laisses <laisse@gmail.com>
2022-11-15 20:05:44 -0300

    routes.js: create authentication middleware

--- src/routes.js
+++ src/routes.js
@@ -57,4 +57,29 @@ export const routes = (app, db) => {
 
         res.status(200).send({ token });
     });
+
+    const authMiddleware = async (req, res, next) => {
+        const { authorization } = req.headers;
+        const token = authorization?.replace("Bearer ", "");
+
+        if (!token) {
+            return res.sendStatus(401);
+        }
+
+        const activeSession = await sessions.findOne({ token });
+        const activeUser = await users.findOne({ _id: activeSession?.userId });
+
+        if (!activeUser) {
+            return res.sendStatus(401);
+        }
+
+        delete activeUser.password;
+
+        req.activeUser = activeUser;
+
+        return next();
+    };
+
+    app.use(authMiddleware);
+
 };
