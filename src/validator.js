de142e1288422171e972a4ed4ec419de2ef62217
Laisses <laisse@gmail.com>
2022-11-15 17:18:47 -0300

    validator.js: create user schema to validation

---
+++ src/validator.js
@@ -0,0 +1,15 @@
+import Joi from "joi";
+
+const validator = schema => payload =>
+    schema.validate(payload, { abortEarly: false });
+
+const userSchema = Joi.object({
+    username: Joi.string().min(1).required(),
+    email: Joi.string().email().required(),
+    password: Joi.string().required(),
+    repeat_password: Joi.ref("password").required()
+});
+
+export const validateUser = validator(userSchema);
+
+//Joi.valid("message", "private_message")
