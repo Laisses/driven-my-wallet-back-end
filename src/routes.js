0ee92748481ce05d38a4fa59189d177f50990a73
Laisses <laisse@gmail.com>
2022-11-15 17:58:59 -0300

    routes.js: create sign-up route

--- src/routes.js
+++ src/routes.js
@@ -1,1 +1,34 @@
-export const routes = () => {};
+import bcrypt from "bcrypt";
+import { validateUser } from "./validator.js";
+
+export const routes = (app, db) => {
+    const users = db.collection("users");
+
+    app.post("/sign-up", async (req, res) => {
+        const { username, email, password } = req.body;
+
+        const userExists = await users.findOne({ "email": email });
+
+        if (userExists) {
+            return res.status(409).send({ message: "this email is already registered" });
+        }
+
+        const { error } = validateUser(req.body);
+
+        if (error) {
+            const errors = error.details.map(detail => detail.message);
+            return res.status(400).send(errors);
+        }
+
+        const hashPassword = bcrypt.hashSync(password, 10);
+
+        await users.insertOne({
+            username,
+            email,
+            password: hashPassword
+        });
+
+        res.sendStatus(201);
+    });
+
+};
--- src/validator.js
+++ src/validator.js
@@ -7,7 +7,7 @@ const userSchema = Joi.object({
     username: Joi.string().min(1).required(),
     email: Joi.string().email().required(),
     password: Joi.string().required(),
-    repeat_password: Joi.ref("password").required()
+    repeat_password: Joi.ref("password")
 });
 
 export const validateUser = validator(userSchema);
